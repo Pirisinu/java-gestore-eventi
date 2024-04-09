d10d22e2debe46f27ca85fc08744a5f2f3102cc1
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-09 12:51:33 +0200

    add Getter e Setter

--- src/org/experis/gestoreEventi/Concert.java
+++ src/org/experis/gestoreEventi/Concert.java
@@ -1,10 +1,34 @@
 package org.experis.gestoreEventi;
 
-public class Concert {
+import java.time.LocalDate;
+
+public class Concert extends Event{
     // Attributes
-    private LocalTime ora;
-    private BigDecimal prezzo;
+    private LocalTime time;
+    private BigDecimal price;
+
     // Constructor
+    public Concert(String title, LocalDate date, int totalSeats, LocalTime time, BigDecimal price) {
+        super(title, date, totalSeats);
+        this.time = time;
+        this.price = price;
+    }
     // Getter e Setter
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
     // Methods
 }
