a0ab50c47f490a5a7026070798ac3c4fa8c51fd3
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-09 12:18:40 +0200

    add EventException

---
+++ src/org/experis/gestoreEventi/excetpion/EventException.java
@@ -0,0 +1,8 @@
+package org.experis.gestoreEventi.excetpion;
+
+public class EventException extends RuntimeException {
+    public EventException(String message) {
+        super(message);
+    }
+}
+
