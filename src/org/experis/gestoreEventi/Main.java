bb6292a22c339df839d8438b020a2964c25dd787
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-09 11:01:58 +0200

    TEST in Main class = loop for quest to user

--- src/org/experis/gestoreEventi/Main.java
+++ src/org/experis/gestoreEventi/Main.java
@@ -17,6 +17,19 @@ public class Main {
             String response = scan.nextLine();
             switch (response.toUpperCase()){
                 case "Y":
+                    System.out.print("Insert event name: ");
+                    String eventName = scan.nextLine();
+                    System.out.print("Insert event year: ");
+                    int eventYear = Integer.parseInt(scan.nextLine());
+                    System.out.print("Insert event month: ");
+                    int eventMonth = Integer.parseInt(scan.nextLine());
+                    System.out.print("Insert event day: ");
+                    int eventDay = Integer.parseInt(scan.nextLine());
+                    System.out.print("Insert event total seats: ");
+                    int eventTotalSeats = Integer.parseInt(scan.nextLine());
+
+                    Event event = new Event(eventName, LocalDate.of(eventYear,eventMonth,eventDay), eventTotalSeats);
+                    System.out.println(event);
                     break;
                 case "N":
                     exit = true;
