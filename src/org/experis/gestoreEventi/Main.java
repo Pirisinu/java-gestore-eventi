e300a2d5dd02433c760b28f3e1ebef5b39b4a911
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-09 11:57:22 +0200

    add loop for quest how many reservation

--- src/org/experis/gestoreEventi/Main.java
+++ src/org/experis/gestoreEventi/Main.java
@@ -19,13 +19,28 @@ public class Main {
                 case "Y":
                     Event event = createNewEvent(scan);
                     System.out.println(event);
-                    System.out.println("How many reservations do you want to make?");
-                    int numOfReservations = Integer.parseInt(scan.nextLine());
-                    if (numOfReservations <= (event.getTotalSeats() - event.getReservedSeats())) {
-                        System.out.println("adding");
-                    } else {
-                        System.out.println("The number of reservations exceeds the available seats.");
-                    }
+                    do {
+                        System.out.println("How many reservations do you want to make?");
+                        int numOfReservations = Integer.parseInt(scan.nextLine());
+                        int successfulReservations = 0;
+                        if (numOfReservations <= (event.getTotalSeats() - event.getReservedSeats())) {
+                            for (int i = 0; i < numOfReservations; i++) {
+                                boolean success = event.reservation();
+                                if (success) {
+                                    successfulReservations++;
+                                } else {
+                                    System.out.println("No more seats available for reservation.");
+                                    break;
+                                }
+                            }
+                        } else {
+                            System.out.println("The number of reservations exceeds the available seats.");
+                        }
+                        System.out.println("Added " + successfulReservations + " reservations.");
+                        System.out.println("Available seats: " + (event.getTotalSeats() - event.getReservedSeats()));
+                        System.out.println("Do you want to make more reservations for this event? (Y/N)");
+                        response = scan.nextLine();
+                    } while (response.equalsIgnoreCase("Y"));
                     break;
                 case "N":
                     exit = true;
