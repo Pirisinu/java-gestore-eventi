28b4fc9e930f7922ced5c30d24e170f67b92416b
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-09 22:13:50 +0200

    add EventsProgram class

---
+++ src/org/experis/gestoreEventi/EventsProgram.java
@@ -0,0 +1,12 @@
+package org.experis.gestoreEventi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventsProgram {
+    // Attributes
+    // Constructor
+    // Getter e Setter
+    // Methods
+
+}
