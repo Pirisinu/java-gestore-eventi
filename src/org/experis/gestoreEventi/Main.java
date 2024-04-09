71b79183c3397ea1a56c6568f998e23946999010
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-09 10:55:02 +0200

    add in Main class = loop for quest to user

--- src/org/experis/gestoreEventi/Main.java
+++ src/org/experis/gestoreEventi/Main.java
@@ -1,13 +1,29 @@
 package org.experis.gestoreEventi;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the event manager");
+        boolean exit = false;
+        Scanner scan = new Scanner(System.in);
+//        //Test
+//        Event event = new Event("pippo", LocalDate.of(2025,10,10),-10);
+//        System.out.println(event);
 
-        //Test
-        Event event = new Event("pippo", LocalDate.of(2025,10,10),-10);
-        System.out.println(event);
+        while (!exit){
+            System.out.println("Do you want insert a new event?");
+            String response = scan.nextLine();
+            switch (response.toUpperCase()){
+                case "Y":
+                    break;
+                case "N":
+                    exit = true;
+                    break;
+            }
+        }
+
+
     }
 }
