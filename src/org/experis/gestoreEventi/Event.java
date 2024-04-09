e918f2aae166145677d9bea227d308c2d465e0bd
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-09 10:06:47 +0200

    add in Event class = Getter

--- src/org/experis/gestoreEventi/Event.java
+++ src/org/experis/gestoreEventi/Event.java
@@ -1,4 +1,44 @@
 package org.experis.gestoreEventi;
 
+import java.time.LocalDate;
+
 public class Event {
+    // Attributes
+    String title;
+    LocalDate date;
+    int totaltSeats, reservedSeats;
+
+    // Constructor
+    // TODO change Exception
+    public Event(String title, LocalDate date, int totaltSeats) {
+        this.title = title;
+        if (LocalDate.now().isBefore(date)){
+            throw new IllegalArgumentException("Enter a date later than today.");
+        } else this.date = date;
+
+        if (totaltSeats < 0){
+            throw new IllegalArgumentException("Enter a number greater than 0");
+        } else this.reservedSeats = 0;
+    }
+    // Getter
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public int getTotaltSeats() {
+        return totaltSeats;
+    }
+
+    public int getReservedSeats() {
+        return reservedSeats;
+    }
+
+    // Setter
+
+    // Methods
 }
